99425c1dbceeaae772e8faee3e594b90cdbd1b90
Pedro Santos <pedro.henrique@construsitebrasil.com.br>
2022-07-11 16:07:35 -0300

    Listagem principal

--- src/components/List/Rentals/index.tsx
+++ src/components/List/Rentals/index.tsx
@@ -3,18 +3,33 @@ import styles from "./styles.module.scss";
 export default function ListRentals() {
     return (
         <>
-            <div className={`${styles.container}`}> 
+            <div className={`${styles.container}`}>
                 <table>
-                    <thead> 
-                        <th> </th> 
-                        <th> Cliente </th>
-                        <th> Valor </th>
-                        <th> Data de Entrega </th>
-                        <th> Horário </th>
-                        <th> Ações </th>
-                        <th>  </th>
+                    <thead className={`${styles.header}`}>
+                        <tr>
+                            <th> </th>
+                            <th> Cliente </th>
+                            <th> Valor </th>
+                            <th> Data de Entrega </th>
+                            <th> Horário </th>
+                            <th> Ações </th>
+                            <th>  </th>
 
+                        </tr>
                     </thead>
+                    <tbody className={`${styles.body}`}>
+                        <tr className={`${styles.item}`}>
+                            <td> </td>
+                            <td>123 </td>
+                            <td>123 </td>
+                            <td>123 </td>
+                            <td>123 </td>
+                            <td>123 </td>
+                            <td> </td>
+                        </tr>
+                    </tbody>
+
+
                 </table>
             </div>
         </>
