63263ea51644930be35824dadf0bb18a77754884
Pedro Santos <pedro.henrique@construsitebrasil.com.br>
2022-08-04 11:45:23 -0300

    Variaveis ambiente next config

--- next.config.js
+++ next.config.js
@@ -7,4 +7,8 @@ module.exports = {
   images: {
       domains: [ 'localhost','festival-party-api.s3.sa-east-1.amazonaws.com'],
   },
+  env: {
+    NEXT_PUBLIC_API_URL_IMAGES: process.env.NEXT_PUBLIC_API_URL_IMAGES,
+    API_URL: process.env.API_URL,
+  }
 }
